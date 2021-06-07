7e790d35de0758c9041e847a3d16a343be4f5a4c
Kirill Borisenko <kirill_borisenko@epam.com>
2021-06-07 21:41:12 +0400

    Unit tests was added to repository

---
+++ RD_XT_NET_WEB_CI/RD_XT_NET_WEB_CI/Classes/Calculater.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RD_XT_NET_WEB_CI.Classes
+{
+    public class Calculater : ICalculater<int>
+    {
+        public int Difference(int firstElem, int secondElem)
+        {
+            return firstElem - secondElem;
+        }
+
+        public int Summ(int firstElem, int secondElem)
+        {
+            return firstElem + secondElem;
+        }
+    }
+}
---
+++ RD_XT_NET_WEB_CI/RD_XT_NET_WEB_CI/Classes/ICalculater.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RD_XT_NET_WEB_CI.Classes
+{
+    public interface ICalculater<T>
+    {
+        T Summ(T firstElem, T secondElem);
+        T Difference(T firstElem, T secondElem);
+    }
+}
