514a358444140ab3983c9d5bf991e101796a08b4
Kirill Borisenko <kirill_borisenko@epam.com>
2021-06-07 20:33:54 +0400

    Simple .NET aplication was added

---
+++ RD_XT_NET_WEB_CI/RD_XT_NET_WEB_CI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RD_XT_NET_WEB_CI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
