db46feb38ea1cc0868e2d8712093673f4d8d0904
Kirill Borisenko <Kirill_Borisenko@epam.com>
2021-06-09 17:22:15 +0400

    [special]: change for testing

--- RD_XT_NET_WEB_CI/RD_XT_NET_WEB_CI/Program.cs
+++ RD_XT_NET_WEB_CI/RD_XT_NET_WEB_CI/Program.cs
@@ -6,7 +6,10 @@ namespace RD_XT_NET_WEB_CI
     {
         static void Main(string[] args)
         {
+            var text = Console.ReadLine();
             Console.WriteLine("Hello World!");
+
+            Console.WriteLine(text);
         }
     }
 }
